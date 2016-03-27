ec5175afff2ad96e2060f9f9fcfb43d16fbfcc68
Margarita <margarita@localhost.localdomain>
2016-03-27 21:58:18 +0300

    change the name of the method from createRandomMinis to generateMinis.  -implement player choices for block(row and column)

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Choose the board size : ");
         System.out.println(" .....  Easy ; Medium ;Hard ");
 
-        int size = 8; //input.nextInt();
+        int size = 3; //input.nextInt();
         String choice = "easy";//input.next().trim().toLowerCase();
 
         char[][] board = new char[size][size];
@@ -37,13 +37,37 @@ public class Main {
                 throw new IllegalArgumentException("Choose between this three choices");
         }
 
-        createRandomMinis(board, minis, size);
+        generateMinis(board, minis, size);
         System.out.println();
         createBoard(board);
+
+        boolean isLive = true;
+        while (isLive) {
+            int moves = blocks - minis;
+            while (moves > 0) {
+                System.out.println("Choose a row:");
+                int row = input.nextInt();
+                System.out.println("Choose a column:");
+                int col = input.nextInt();
+
+                if (board[row][col] == bomb) {
+                    System.out.println("You are the next kamikadze! :D ");
+                    isLive = false;
+                    break;
+                } else if (board[row][col] == 'V') {
+                    System.out.println("You can't twice or choose the same square./nThis will take you one live.");
+                    moves--;
+                } else {
+                    board[row][col] = 'V';
+                }
+
+                createBoard(board);
+            }
+        }
+
     }
 
-    private static char[][] createRandomMinis(char[][] board, int minis, int size) {
-
+    private static char[][] generateMinis(char[][] board, int minis, int size) {
         Random rnd = new Random();
 
         while (minis > 0) {
@@ -60,7 +84,7 @@ public class Main {
     public static void createBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                System.out.print("X" + board[i][j]);
+                System.out.print("_" + board[i][j]);
             }
             System.out.println();
         }
