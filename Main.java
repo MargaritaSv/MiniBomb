8b64f076e38abf98011c1e8061e75cc1435024a0
Margarita <margarita@localhost.localdomain>
2016-03-27 22:37:09 +0300

    voala

--- Main.java
+++ Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final char bomb = '@';
+    private static final char BOMB = '@';
+    private static final boolean SEE = false;
 
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Choose the board size : ");
-        System.out.println(" .....  Easy ; Medium ;Hard ");
 
-        int size = 3; //input.nextInt();
-        String choice = "easy";//input.next().trim().toLowerCase();
+        System.out.println("Choose the board size : ");
+        int size = input.nextInt();
+
+        System.out.println("What level you wish to play?/n-Easy /n-Medium/n-Hard");
+        String choice = input.next().trim().toLowerCase();
 
         char[][] board = new char[size][size];
         createBoard(board);
@@ -39,7 +41,7 @@ public class Main {
 
         generateMinis(board, minis, size);
         System.out.println();
-        createBoard(board);
+        showBoard(board);
 
         boolean isLive = true;
         while (isLive) {
@@ -50,7 +52,7 @@ public class Main {
                 System.out.println("Choose a column:");
                 int col = input.nextInt();
 
-                if (board[row][col] == bomb) {
+                if (board[row][col] == BOMB) {
                     System.out.println("You are the next kamikadze! :D ");
                     isLive = false;
                     break;
@@ -60,11 +62,9 @@ public class Main {
                 } else {
                     board[row][col] = 'V';
                 }
-
-                createBoard(board);
+                showBoard(board);
             }
         }
-
     }
 
     private static char[][] generateMinis(char[][] board, int minis, int size) {
@@ -73,22 +73,41 @@ public class Main {
         while (minis > 0) {
             int row = rnd.nextInt(size);
             int col = rnd.nextInt(size);
-            if (board[row][col] != bomb) {
-                board[row][col] = bomb;
+            if (board[row][col] != BOMB) {
+                board[row][col] = BOMB;
                 minis--;
             }
         }
         return board;
     }
 
-    public static void createBoard(char[][] board) {
+    public static void showBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                System.out.print("_" + board[i][j]);
+            if (SEE) {
+                for (int j = 0; j < board[0].length; j++) {
+                    System.out.print("_" + board[i][j]);
+                }
+                System.out.println();
+
+            } else {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == BOMB) {
+                        System.out.print("_" + " ");
+                    } else {
+                        System.out.print("_" + board[i][j]);
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 
+    public static void createBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
 }
 
